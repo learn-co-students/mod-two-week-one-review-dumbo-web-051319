5c647faa87d642f73492e6e8fe1970d19b02d454
L.R.L <DaleLoreNY@gmail.com>
2019-06-07 17:51:24 -0400

    CRUD-ed seas

--- Rails-Seven-Seas/app/controllers/seas_controller.rb
+++ Rails-Seven-Seas/app/controllers/seas_controller.rb
@@ -1,8 +1,52 @@
 class SeasController < ApplicationController
   #define your controller actions here
+  def welcome
+  end
+
+  def index
+    @seas = Sea.all
+  end
+
+  def create
+    new_sea = Sea.create(sea_params)
+    # redirect_to sea_path(new_sea)
+    redirect_to new_sea
+    # redirect_to :"/seas/#{new_sea.id}" <-not the rails way
+  end
+
+  def new
+    @sea = Sea.new
+  end
+
+  def edit 
+    @sea = Sea.find(params[:id])
+  end
+
+  def show 
+    @sea = Sea.find(params[:id])
+  end
+ 
+  def update
+    @sea = Sea.find(params[:id])
+    @sea.update(sea_params)
+    redirect_to @sea
+  end
+
+  def delete
+    @sea = Sea.find(params[:id])
+    @sea.destroy
+  end
+  # patch "/seas/:id" do
+  #   @sea = Sea.find(params[:id])
+  #   params.delete("_method")
+  #   params[:has_mermaids] ||= false
+  #   @sea.update(params)
+  #   redirect to "/seas/#{@sea.id}"
+  # end
 
 
-  private
+
+  private #<----fcuk yiu
   # In controller actions, use this private method to access sea params from forms.
   # Example: @sea.update(sea_params)
   # check the return value of sea_params to see what you're working with!
